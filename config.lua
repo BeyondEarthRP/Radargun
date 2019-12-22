38a58e412ca77cb0e7112901b0eef5674ca2ea90
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-22 09:55:32 +0000

    changed from kph to mph

--- config.lua
+++ config.lua
@@ -10,5 +10,5 @@ cfg = {}
 
 cfg.menuopen = 246 -- BUTTON TO OPEN MENU (https://docs.fivem.net/game-references/controls/)
 cfg.bottomfreeze = 38 -- FREEZE (https://docs.fivem.net/game-references/controls/)
-cfg.metric = false --// IF METRIC = FALSE THEN IT WILL SHOW AS KM/H, IF METRIC = TRUE, THEN IT WILL CHANGE TO MPH
+cfg.metric = true --// IF METRIC = FALSE THEN IT WILL SHOW AS KM/H, IF METRIC = TRUE, THEN IT WILL CHANGE TO MPH
 cfg.radargun = "WEAPON_VINTAGEPISTOL"
